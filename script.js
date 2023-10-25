c9f45d907aa2dca421c9ae782a8a435978d2f22e
snejlime <38116855+limesnej@users.noreply.github.com>
2023-10-25 23:10:36 +0200

    Add border around the vertical divs

--- script.js
+++ script.js
@@ -7,17 +7,16 @@ let numberOfGrids = 16;
 let arrayDiv = new Array();
 let horizArrayDiv = new Array();
 container.style.cssText = "display: flex;"
-for (let i = 0; i <= 16; i++)
+for (let i = 0; i < 16; i++)
 {
     arrayDiv[i] = document.createElement("div");
-    arrayDiv[i].innerHTML = "SHIT";
-    arrayDiv[i].style.cssText = "flex: 1;";
+    arrayDiv[i].style.cssText = "flex: 1 ";
     container.appendChild(arrayDiv[i]);
     for (let j= 0; j<= 16; j++)
     {
         horizArrayDiv[j] = document.createElement("div");
-        horizArrayDiv[j].innerHTML = "poop";
-        horizArrayDiv[j].style.cssText = "display: ";
+        // horizArrayDiv[j].innerHTML = "poop";
+        horizArrayDiv[j].style.cssText = "flex: 1; border: 2px solid black; padding: 20px;";
         arrayDiv[i].appendChild(horizArrayDiv[j]);
     }
 }
