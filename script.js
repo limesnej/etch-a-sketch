d12d39fb6135faddadee77b45403b1d60a0cb3f4
snejlime <38116855+limesnej@users.noreply.github.com>
2023-11-08 22:16:54 +0100

    Add foreach loop to draw red

--- script.js
+++ script.js
@@ -12,11 +12,23 @@ for (let i = 0; i < 16; i++)
     arrayDiv[i] = document.createElement("div");
     arrayDiv[i].style.cssText = "flex: 1 ";
     container.appendChild(arrayDiv[i]);
+    
     for (let j= 0; j<= 16; j++)
     {
         horizArrayDiv[j] = document.createElement("div");
         // horizArrayDiv[j].innerHTML = "poop";
-        horizArrayDiv[j].style.cssText = "flex: 1; border: 2px solid black; padding: 20px;";
+        
+        horizArrayDiv[j].style.cssText = "flex: 1; border: 1px solid black; padding: 10px;";
+        horizArrayDiv[j].setAttribute("id", `mid`);
         arrayDiv[i].appendChild(horizArrayDiv[j]);
+        
     }
 }
+
+const myDivs = document.querySelectorAll("#mid");
+
+[...myDivs].forEach(mid => {
+    mid.addEventListener("mouseover", () => {
+        mid.style.backgroundColor = "red";
+    })
+})
